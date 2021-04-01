0793b9c6513576dcc9db117edfcce3ebb7c3a3b5
Okura Ikeda <okura@fantom.foundation>
2021-04-01 07:31:06 -0300

    marketplace listing testcase

---
+++ test/FantomMarketplace.test.js
@@ -0,0 +1,93 @@
+const {
+    BN,
+    constants,
+    expectEvent,
+    expectRevert,
+} = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+const { expect } = require('chai');
+
+const FantomNFT = artifacts.require('FantomNFT');
+const FantomMarketplace = artifacts.require('FantomMarketplace');
+
+contract('Core ERC721 tests for FantomNFT', function ([
+    owner,
+    minter,
+    approved,
+    anotherApproved,
+    operator,
+    other,
+    artist,
+    feeRecipient,
+]) {
+    const name = 'Fantom NFT';
+    const symbol = 'FNFT';
+  
+    const firstTokenId = new BN('1');
+    const secondTokenId = new BN('2');
+    const nonExistentTokenId = new BN('99');
+    const platformFee = new BN('25'); // marketplace platform fee: 2.5%
+    const pricePerItem = new BN('1000000000000000000');
+  
+    const RECEIVER_MAGIC_VALUE = '0x150b7a02';
+  
+    const randomTokenURI = 'ipfs';
+  
+    beforeEach(async function () {
+      this.nft = await FantomNFT.new({ from: owner });
+      this.marketplace = await FantomMarketplace.new(
+        feeRecipient,
+        platformFee,
+        { from: owner }
+      );
+      this.nft.mint(minter, randomTokenURI, { from: owner });
+      this.nft.mint(owner, randomTokenURI, { from: owner });
+    });
+
+    describe('Listing Item', function () {
+        it('reverts when not owning NFT', async function() {
+            await expectRevert(
+                this.marketplace.listItem(
+                    this.nft.address,
+                    firstTokenId,
+                    '1',
+                    pricePerItem,
+                    '0',
+                    ZERO_ADDRESS,
+                    { from: owner }
+                ),
+                "Must be owner of NFT."
+            );
+        });
+
+        it('reverts when not approved', async function() {
+            await expectRevert(
+                this.marketplace.listItem(
+                    this.nft.address,
+                    firstTokenId,
+                    '1',
+                    pricePerItem,
+                    '0',
+                    ZERO_ADDRESS,
+                    { from: minter }
+                ),
+                "Must be approved before list."
+            );
+        });
+
+        it('successfuly lists item', async function() {
+            await this.nft.setApprovalForAll(this.marketplace.address, true, { from: minter });
+            await this.marketplace.listItem(
+                this.nft.address,
+                firstTokenId,
+                '1',
+                pricePerItem,
+                '0',
+                ZERO_ADDRESS,
+                { from: minter }
+            );
+        })
+    });
+  
+})
