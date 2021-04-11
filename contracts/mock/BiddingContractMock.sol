28d9937419eb4db2c557fc17374129b965f46850
Okura Ikeda <okura@fantom.foundation>
2021-04-11 19:59:00 -0300

    mock contracts

---
+++ contracts/mock/BiddingContractMock.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "../FantomAuction.sol";
+
+contract BiddingContractMock {
+    FantomAuction public auctionContract;
+
+    constructor(FantomAuction _auctionContract) public {
+        auctionContract = _auctionContract;
+    }
+
+    function bid(address _nftAddress, uint256 _tokenId) external payable {
+        auctionContract.placeBid{value: msg.value}(_nftAddress, _tokenId);
+    }
+}
