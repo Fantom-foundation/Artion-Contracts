f35656089ac63ca7e7e8b5ffa337e11b51067043
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-09-16 19:31:16 +0400

    Update New TREASURY_ADDRESS

--- scripts/10_deploy_1155.js
+++ scripts/10_deploy_1155.js
@@ -1,22 +1,26 @@
+const { TREASURY_ADDRESS } = require('./constants');
+
 async function main() {
-  const ArtTradable = await ethers.getContractFactory('FantomArtTradable')
+  const ArtTradable = await ethers.getContractFactory('FantomArtTradable');
   const nft = await ArtTradable.deploy(
     'FantomArt',
     'FART',
     '20000000000000000000',
-    '0x9B2Bb6290fb910a960Ec344cDf2ae60ba89647f6',
+    TREASURY_ADDRESS,
     '0x19fD7C9B72cd944f987E0aB1FdD33fF8f68Cf87C',
     '0x0EeB6B95B52dfDFb86CcF960F8408a211555b63b'
   );
   await nft.deployed();
   console.log('FantomArt deployed to:', nft.address);
 
-  const ArtTradablePrivate = await ethers.getContractFactory('FantomArtTradablePrivate')
+  const ArtTradablePrivate = await ethers.getContractFactory(
+    'FantomArtTradablePrivate'
+  );
   const nftPrivate = await ArtTradablePrivate.deploy(
     'FantomArt',
     'FART',
     '20000000000000000000',
-    '0x9B2Bb6290fb910a960Ec344cDf2ae60ba89647f6',
+    TREASURY_ADDRESS,
     '0x19fD7C9B72cd944f987E0aB1FdD33fF8f68Cf87C',
     '0x0EeB6B95B52dfDFb86CcF960F8408a211555b63b'
   );
@@ -27,8 +31,8 @@ async function main() {
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-.then(() => process.exit(0))
-.catch((error) => {
-  console.error(error)
-  process.exit(1)
-})
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/6_deploy_test_nft.js
+++ scripts/6_deploy_test_nft.js
@@ -1,3 +1,5 @@
+const { TREASURY_ADDRESS } = require('./constants');
+
 async function main() {
   // const NFTTradable = await ethers.getContractFactory('FantomNFTTradable')
   // const nft = await NFTTradable.deploy(
@@ -12,7 +14,9 @@ async function main() {
   // await nft.deployed();
   // console.log('Artion deployed to:', nft.address);
 
-  const NFTTradablePrivate = await ethers.getContractFactory('FantomNFTTradablePrivate')
+  const NFTTradablePrivate = await ethers.getContractFactory(
+    'FantomNFTTradablePrivate'
+  );
   const nftPrivate = await NFTTradablePrivate.deploy(
     'IArtion',
     'IART',
@@ -20,7 +24,7 @@ async function main() {
     '0x19fD7C9B72cd944f987E0aB1FdD33fF8f68Cf87C',
     '0x0EeB6B95B52dfDFb86CcF960F8408a211555b63b',
     '10000000000000000000',
-    '0x9B2Bb6290fb910a960Ec344cDf2ae60ba89647f6'
+    TREASURY_ADDRESS
   );
   await nftPrivate.deployed();
   console.log('IArtion deployed to:', nftPrivate.address);
@@ -29,8 +33,8 @@ async function main() {
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-.then(() => process.exit(0))
-.catch((error) => {
-  console.error(error)
-  process.exit(1)
-})
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
