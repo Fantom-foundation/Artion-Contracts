56a8815c55d03982601a3e78a009f1724ffc06a0
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-11-01 14:08:01 +0400

    Update configuration and package/package-versions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-truffle5');
-require("@nomiclabs/hardhat-etherscan");
+require('@nomiclabs/hardhat-etherscan');
 require('hardhat-gas-reporter');
 require('solidity-coverage');
 require('@nomiclabs/hardhat-solhint');
@@ -12,18 +12,18 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: {
-    version: '0.6.12',
+    version: '0.8.0',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
+        runs: 200
+      }
+    }
   },
   gasReporter: {
     currency: 'USD',
     enabled: false,
-    gasPrice: 50,
+    gasPrice: 50
   },
   networks: {
     mainnet: {
@@ -34,20 +34,20 @@ module.exports = {
     testnet: {
       url: `https://rpc.testnet.fantom.network`,
       chainId: 4002,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${PRIVATE_KEY}`]
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`,
       chainId: 3,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${PRIVATE_KEY}`]
     },
     coverage: {
-      url: 'http://localhost:8555',
+      url: 'http://localhost:8555'
     },
-    
+
     localhost: {
       url: `http://127.0.0.1:8545`
-    },
+    }
   },
   etherscan: {
     apiKey: '46DD6NK19R2AZQQIJIY1FXR85HKM2XSNBE'
