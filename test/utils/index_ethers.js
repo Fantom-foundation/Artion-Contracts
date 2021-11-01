7ba750f4fe33e5987ad98443b3fbf2512d67e7da
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-11-01 15:45:34 +0400

    Reformatting

--- test/utils/index_ethers.js
+++ test/utils/index_ethers.js
@@ -1,25 +1,25 @@
-const { BigNumber } = require("ethers");
+const { BigNumber } = require('ethers');
 
 // Set global variables
-const ZERO = new BigNumber.from("0");
-const ONE = new BigNumber.from("1");
-const TWO = new BigNumber.from("2");
-const THREE = new BigNumber.from("3");
-const FOUR = new BigNumber.from("4");
-const FIVE = new BigNumber.from("5");
-const SIX = new BigNumber.from("6");
-const SEVEN = new BigNumber.from("7");
-const EIGHT = new BigNumber.from("8");
-const NINE = new BigNumber.from("9");
+const ZERO = new BigNumber.from('0');
+const ONE = new BigNumber.from('1');
+const TWO = new BigNumber.from('2');
+const THREE = new BigNumber.from('3');
+const FOUR = new BigNumber.from('4');
+const FIVE = new BigNumber.from('5');
+const SIX = new BigNumber.from('6');
+const SEVEN = new BigNumber.from('7');
+const EIGHT = new BigNumber.from('8');
+const NINE = new BigNumber.from('9');
 
 // Set MockERC20 variables
-const mockPayTokenName = "Fantom";
-const mockPayTokenSymbol = "FTM";
-const mockPayTokenMintAmount = new BigNumber.from("2000000000000000000000"); // Sets test accounts to 2000 FTM
+const mockPayTokenName = 'Fantom';
+const mockPayTokenSymbol = 'FTM';
+const mockPayTokenMintAmount = new BigNumber.from('2000000000000000000000'); // Sets test accounts to 2000 FTM
 
 // Set MockERC721 variables
-const mockNFTokenName = "Test NFT";
-const mockNFTokenSymbol = "NFT";
+const mockNFTokenName = 'Test NFT';
+const mockNFTokenSymbol = 'NFT';
 
 module.exports = {
   ZERO,
@@ -36,5 +36,5 @@ module.exports = {
   mockPayTokenSymbol,
   mockPayTokenMintAmount,
   mockNFTokenName,
-  mockNFTokenSymbol,
+  mockNFTokenSymbol
 };
