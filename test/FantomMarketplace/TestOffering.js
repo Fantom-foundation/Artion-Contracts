8b141c7396b3eed67a7a62c56e5f125e3f54c6b7
prolingua <prolingua.geo@gmail.com>
2021-11-03 07:46:38 +0000

    more updates on TestBuying.js and created TestOffering.js

--- test/FantomMarketplace/TestOffering.js
+++ test/FantomMarketplace/TestOffering.js
@@ -141,107 +141,121 @@ contract('FantomMarketplace - Buying Test', function([
       new BN('1632304800'), // 2021-09-22 10:00:00 GMT
       { from: artist }
     );
+
+    await this.mockERC721.setApprovalForAll(this.fantomAuction.address, true, {
+      from: artist
+    });
+
+    await this.fantomAuction.createAuction(
+      this.mockERC721.address,
+      ONE,
+      this.mockERC20.address,
+      ether('20'),
+      new BN('1632564000'), //2021-09-25 10:00:00
+      false,
+      new BN('1632996000'), //2021-09-30 10:00:00
+      { from: artist }
+    );
   });
 
-  it('A buyer tries to buy an nft which is not listed. He/She will fail with "not listed item"', async function() {
+  it(`A buyer makes an offer for a listed nft with the deadline in the past. 
+      He/She will fail with "invalid expiration"`, async function() {
+    //Let's mock that the current time:2021-09-22 11:00:00 GMT
+    await this.fantomMarketplace.setTime(new BN('1632308400'));
+
+    //The buyer makes an offer for the nft for 18 wFTMs with deadline on 2021-09-22 10:00:00 GMT
     await expectRevert(
-      this.fantomMarketplace.buyItem(
+      this.fantomMarketplace.createOffer(
+        this.mockERC721.address,
+        ZERO,
+        this.mockERC20.address,
+        ONE,
+        ether('18'),
+        new BN('1632304800'),
+        { from: buyer }
+      ),
+      'invalid expiration'
+    );
+  });
+
+  it(`A buyer makes an offer a nft which is in auction. 
+      He/She will fail with "cannot place an offer if auction is going on"`, async function() {
+    //Let's mock that the current time:2021-09-22 11:00:00 GMT
+    await this.fantomMarketplace.setTime(new BN('1632308400'));
+
+    //The buyer makes an offer for the nft for 18 wFTMs with deadline on 2021-09-22 15:00:00 GMT
+    await expectRevert.unspecified(
+      this.fantomMarketplace.createOffer(
         this.mockERC721.address,
         ONE,
         this.mockERC20.address,
-        artist,
+        ONE,
+        ether('18'),
+        new BN('1632322800'),
         { from: buyer }
-      ),
-      'not listed item'
+      )
     );
+
+    //.TODO : this should work but it doesn't
+    /*  await expectRevert(
+        this.fantomMarketplace.createOffer(
+          this.mockERC721.address,
+          ONE,
+          this.mockERC20.address,
+          ONE,
+          ether('18'),
+          new BN('1632322800'),
+          { from: buyer }
+        ), 'cannot place an offer if auction is going on'
+      ); */
   });
 
-  it('A buyer tries to buy a listed nft with an invalid pay token. He/She will fail with "invalid pay token"', async function() {
-    //Let's mock that the current time:2021-09-22 11:00:00 GMT
-    await this.fantomMarketplace.setTime(new BN('1632308400'));
-    await expectRevert(
-      this.fantomMarketplace.buyItem(
-        this.mockERC721.address,
-        ZERO,
-        this.mockERC20_invalid.address,
-        artist,
-        { from: buyer }
-      ),
-      'invalid pay token'
-    );
-  });
-
-  it('A buyer tries to buy a listed nft which hasnt started. He/She will fail with "item not buyable"', async function() {
-    //Let's mint 20 wFTMs to the buyer so he/she can purchase the nft
-    await this.mockERC20.mintPay(buyer, ether('20'));
+  it(`A buyer makes an offer for a listed nft for a lower price. 
+      OfferCreated event should be emitted with correct values`, async function() {
+    await this.mockERC20.mintPay(buyer, ether('18'));
     //The buyer approve his/her wFTM to the marketplace
-    await this.mockERC20.approve(this.fantomMarketplace.address, ether('20'), {
+    await this.mockERC20.approve(this.fantomMarketplace.address, ether('18'), {
       from: buyer
     });
-    //Let's mock that the current time:2021-09-21 10:00:00 GMT
-    await this.fantomMarketplace.setTime(new BN('1632218400'));
-    await expectRevert(
-      this.fantomMarketplace.buyItem(
-        this.mockERC721.address,
-        ZERO,
-        this.mockERC20.address,
-        artist,
-        { from: buyer }
-      ),
-      'item not buyable'
-    );
-  });
 
-  it('A buyer buys a listed nft. Event ItemSold should be emitted with  correct values', async function() {
     //Let's mock that the current time:2021-09-22 11:00:00 GMT
     await this.fantomMarketplace.setTime(new BN('1632308400'));
 
-    //The buyer purchases the nft
-    result = await this.fantomMarketplace.buyItem(
+    //The buyer makes an offer for the nft for 18 wFTMs with deadline on 2021-09-22 15:00:00 GMT
+    result = await this.fantomMarketplace.createOffer(
       this.mockERC721.address,
       ZERO,
       this.mockERC20.address,
-      artist,
+      ONE,
+      ether('18'),
+      new BN('1632322800'),
       { from: buyer }
     );
 
-    //Event ItemSold should be emitted
-    expectEvent(result, 'ItemSold', {
-      seller: artist,
-      buyer: buyer,
+    //Event OfferCreated should be emitted with correct values
+    expectEvent(result, 'OfferCreated', {
+      creator: buyer,
       nft: this.mockERC721.address,
       tokenId: ZERO,
       quantity: ONE,
-      unitPrice: ZERO,
-      pricePerItem: ether('20')
+      payToken: this.mockERC20.address,
+      pricePerItem: ether('18'),
+      deadline: new BN('1632322800')
     });
   });
 
-  it('The seller should get the correct amount of wFTMs', async function() {
-    wFTMBalance = await this.mockERC20.balanceOf(artist);
-    expect(wFTMBalance.toString()).to.be.equal(ether('19').toString());
-  });
-
-  it('The platform fee recipient should get the correct amount of wFTMs', async function() {
-    wFTMBalance = await this.mockERC20.balanceOf(platformFeeRecipient);
-    expect(wFTMBalance.toString()).to.be.equal(ether('1').toString());
-  });
-
-  it('The nft should no longer on the list', async function() {
-    //The nft shouldn't be on the listing anymore
-    listing = await this.fantomMarketplace.listings(
-      this.mockERC721.address,
-      ZERO,
-      artist
+  it(`The same buyer tries to create the same offer again. He/She will fail with "offer already created"`, async function() {
+    await expectRevert(
+      this.fantomMarketplace.createOffer(
+        this.mockERC721.address,
+        ZERO,
+        this.mockERC20.address,
+        ONE,
+        ether('18'),
+        new BN('1632322800'),
+        { from: buyer }
+      ),
+      'offer already created'
     );
-    expect(listing.quantity.toString()).to.be.equal(ZERO.toString());
-    expect(listing.payToken).to.be.equal(constants.ZERO_ADDRESS);
-    expect(listing.pricePerItem.toString()).to.be.equal(ether('0').toString());
-    expect(listing.startingTime.toString()).to.be.equal(ZERO.toString());
-  });
-
-  it('The owner of the nft should be the buyer now', async function() {
-    nftOwner = await this.mockERC721.ownerOf(ZERO);
-    expect(nftOwner).to.be.equal(buyer);
   });
 });
