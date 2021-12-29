1fa6c61d90ae38dff82d792ecc277590aeea9704
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2021-12-29 15:16:34 +0400

    Add internal functions

--- contracts/FantomRoyaltyRegistry.sol
+++ contracts/FantomRoyaltyRegistry.sol
@@ -5,10 +5,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-import "./library/ERC2981Base.sol";
+import "./library/IERC2981Royalties.sol";
 
-contract FantomRoyaltyRegistry is Ownable, ERC2981Base {
-    address royaltyMigrationManager;
+interface IFantomMarketplace {
+    function collectionRoyalties(address)
+        external
+        view
+        returns (
+            uint16,
+            address,
+            address
+        );
+}
+
+contract FantomRoyaltyRegistry is Ownable {
+    address public royaltyMigrationManager;
+    address public marketplaceOld;
+
+    struct RoyaltyInfo {
+        address receiver;
+        uint16 royaltyPercent;
+    }
+
+    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+
+    // NftAddress -> TokenId -> RoyaltyInfo
+    mapping(address => mapping(uint256 => RoyaltyInfo)) internal _royalties;
 
     modifier auth(address _collection, uint256 _tokenId) {
         require(
@@ -35,6 +57,9 @@ contract FantomRoyaltyRegistry is Ownable, ERC2981Base {
         address _receiver,
         uint16 _royaltyPercent
     ) external auth(_collection, _tokenId) {
+        RoyaltyInfo memory royalty = _royalties[_collection][_tokenId];
+
+        require(royalty.royaltyPercent == 0, "NFT royalty already set");
         require(_royaltyPercent <= 10000, "royalty too high");
 
         _setRoyalty(_collection, _tokenId, _receiver, _royaltyPercent);
@@ -44,17 +69,47 @@ contract FantomRoyaltyRegistry is Ownable, ERC2981Base {
         address _collection,
         uint256 _tokenId,
         uint256 _salePrice
-    )
-        external
-        view
-        override
-        returns (address _receiver, uint256 _royaltyAmount)
-    {
-        (_receiver, _royaltyAmount) = _royaltyInfo(
-            _collection,
-            _tokenId,
-            _salePrice
+    ) external view returns (address _receiver, uint256 _royaltyAmount) {
+        if (IERC165(_collection).supportsInterface(_INTERFACE_ID_ERC2981)) {
+            (_receiver, _royaltyAmount) = IERC2981Royalties(_collection)
+                .royaltyInfo(_tokenId, _salePrice);
+        } else {
+            (_receiver, _royaltyAmount) = _royaltyInfo(
+                _collection,
+                _tokenId,
+                _salePrice
+            );
+        }
+    }
+
+    function _setRoyalty(
+        address _collection,
+        uint256 _tokenId,
+        address _receiver,
+        uint16 _royaltyPercent
+    ) internal {
+        _royalties[_collection][_tokenId] = RoyaltyInfo(
+            _receiver,
+            _royaltyPercent
         );
+    }
+
+    function _royaltyInfo(
+        address _collection,
+        uint256 _tokenId,
+        uint256 _salePrice
+    ) internal view returns (address _receiver, uint256 _royaltyAmount) {
+        RoyaltyInfo memory royalty = _royalties[_collection][_tokenId];
+
+        if (royalty.receiver != address(0) && royalty.royaltyPercent != 0) {
+            _receiver = royalty.receiver;
+            _royaltyAmount = (_salePrice * royalty.royaltyPercent) / 10000;
+        } else {
+            // Fallback version settings come here
+            
+            _receiver = address(0);
+            _royaltyAmount = 0;
+        }
     }
 
     /**
--- contracts/library/IERC2981Royalties.sol
+++ contracts/library/IERC2981Royalties.sol
@@ -2,10 +2,22 @@
 
 pragma solidity ^0.8.0;
 
-/// @dev Interface for the NFT Royalty Standard
-interface IERC2981Royalties {
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+interface IERC2981Royalties is IERC165 {
+    /// ERC165 bytes to add to interface array - set in parent contract
+    /// implementing this standard
+    ///
+    /// bytes4(keccak256("royaltyInfo(uint256,uint256)")) == 0x2a55205a
+    /// bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+    /// _registerInterface(_INTERFACE_ID_ERC2981);
+
+    /// @notice Called with the sale price to determine how much royalty
+    //          is owed and to whom.
+    /// @param _salePrice - the sale price of the NFT asset specified by _tokenId
+    /// @return receiver - address of who should be sent the royalty payment
+    /// @return royaltyAmount - the royalty payment amount for _salePrice
     function royaltyInfo(
-        address _collection,
         uint256 _tokenId,
         uint256 _salePrice
     ) external view returns (address receiver, uint256 royaltyAmount);
