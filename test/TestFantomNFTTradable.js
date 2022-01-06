83475576b5c49e3672ca741ceb49f7fdbc7a1afd
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2022-01-06 17:28:56 +0400

    Add Tests for NFTTradable

---
+++ test/TestFantomNFTTradable.js
@@ -0,0 +1,178 @@
+// npx hardhat test .\test\TestFantomMarketplace_1.js --network localhost; run first in another shell: npx hardhat node
+const { expectRevert, expectEvent, BN } = require('@openzeppelin/test-helpers');
+
+const { expect } = require('chai');
+
+const {
+  ZERO,
+  ONE,
+  TWO,
+  mockPayTokenName,
+  mockPayTokenSymbol,
+  etherToWei,
+  weiToEther
+} = require('./utils/index.js');
+
+const _INTERFACE_ID_ROYALTIES_EIP2981 = '0x2a55205a';
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+const FantomRoyaltyRegistry = artifacts.require('FantomRoyaltyRegistry');
+const MockERC20 = artifacts.require('MockERC20');
+const FantomNFTTradable = artifacts.require('FantomNFTTradable');
+
+contract(
+  'FantomNFTTradable',
+  function ([
+    owner,
+    receiver,
+    tradableManager,
+    platformFeeRecipient,
+    royaltyMigrationManager,
+    other
+  ]) {
+    before(async function () {
+      this.mockERC20 = await MockERC20.new(
+        mockPayTokenName,
+        mockPayTokenSymbol,
+        ZERO
+      );
+
+      this.fantomNFTTradable = await FantomNFTTradable.new(
+        mockPayTokenName,
+        mockPayTokenSymbol,
+        ZERO_ADDRESS,
+        ZERO_ADDRESS,
+        ZERO_ADDRESS,
+        etherToWei(1),
+        platformFeeRecipient,
+        false,
+        ZERO_ADDRESS
+      );
+
+      this.fantomNFTTradablePrivate = await FantomNFTTradable.new(
+        'Artion',
+        'ART',
+        ZERO_ADDRESS,
+        ZERO_ADDRESS,
+        ZERO_ADDRESS,
+        etherToWei(1),
+        platformFeeRecipient,
+        true,
+        tradableManager
+      );
+
+      this.fantomRoyaltyRegistry = await FantomRoyaltyRegistry.new();
+      this.fantomRoyaltyRegistry.updateMigrationManager(
+        royaltyMigrationManager
+      );
+    });
+
+    describe('NFTTradable (Public)', function () {
+      it('It should support IERC2981', async function () {
+        expect(
+          await this.fantomNFTTradable.supportsInterface(
+            _INTERFACE_ID_ROYALTIES_EIP2981
+          )
+        ).to.be.true;
+      });
+
+      it('It should allow minting', async function () {
+        await this.fantomNFTTradable.mint(owner, '', receiver, new BN('100'), {
+          value: etherToWei(1)
+        });
+      });
+
+      it('It should not allow invalid royalty', async function () {
+        await expectRevert(
+          this.fantomNFTTradable.mint(owner, '', receiver, new BN('10001'), {
+            value: etherToWei(1)
+          }),
+          'Royalty Too high'
+        );
+      });
+
+      it('RoyaltyRegistry should return correct details', async function () {
+        let details = await this.fantomRoyaltyRegistry.royaltyInfo(
+          this.fantomNFTTradable.address,
+          ONE,
+          etherToWei(1000)
+        );
+
+        const { 0: _receiver, 1: _royaltyAmount } = details;
+
+        expect(_receiver.toString()).to.be.equal(receiver);
+        expect(weiToEther(_royaltyAmount)).to.be.equal('10'); //1% of 1000
+      });
+
+      it('RoyaltyRegistry should return empty receiver address and zero amount [if not found]', async function () {
+        let details = await this.fantomRoyaltyRegistry.royaltyInfo(
+          this.fantomNFTTradable.address,
+          TWO,
+          etherToWei(1000)
+        );
+
+        const { 0: _receiver, 1: _royaltyAmount } = details;
+
+        expect(_receiver.toString()).to.be.equal(ZERO_ADDRESS);
+        expect(weiToEther(_royaltyAmount)).to.be.equal('0'); //1% of 1000
+      });
+    });
+
+    describe('NFTTradable (Private)', function () {
+      it('It should not allow a random user to mint', async function () {
+        await expectRevert(
+          this.fantomNFTTradablePrivate.mint(
+            owner,
+            '',
+            receiver,
+            new BN('200'),
+            {
+              value: etherToWei(1),
+              from: other
+            }
+          ),
+          'not authorized'
+        );
+      });
+
+      it('It should allow a tradable manager to mint', async function () {
+        await this.fantomNFTTradablePrivate.mint(
+          owner,
+          '',
+          receiver,
+          new BN('200'),
+          {
+            value: etherToWei(1),
+            from: tradableManager
+          }
+        );
+      });
+
+      it('RoyaltyRegistry should return correct details', async function () {
+        let details = await this.fantomRoyaltyRegistry.royaltyInfo(
+          this.fantomNFTTradablePrivate.address,
+          ONE,
+          etherToWei(1000)
+        );
+
+        const { 0: _receiver, 1: _royaltyAmount } = details;
+
+        expect(_receiver.toString()).to.be.equal(receiver);
+        expect(weiToEther(_royaltyAmount)).to.be.equal('20'); //1% of 1000
+      });
+
+      it('RoyaltyRegistry should return empty receiver address and zero amount [if not found]', async function () {
+        let details = await this.fantomRoyaltyRegistry.royaltyInfo(
+          this.fantomNFTTradablePrivate.address,
+          TWO,
+          etherToWei(1000)
+        );
+
+        const { 0: _receiver, 1: _royaltyAmount } = details;
+
+        expect(_receiver.toString()).to.be.equal(ZERO_ADDRESS);
+        expect(weiToEther(_royaltyAmount)).to.be.equal('0'); //1% of 1000
+      });
+    });
+  }
+);
