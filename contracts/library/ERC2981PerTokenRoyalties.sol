556373d86db1c5a50581395cdc35b52c4b81fa4b
Jiri Malek <jirka.malek@gmail.com>
2022-02-01 14:00:38 +0100

    Fix default collection royalty access control.
    The royalty can be set only once and only by the token owner and/or approved operator. Any other access is prohibited.

--- contracts/library/ERC2981PerTokenRoyalties.sol
+++ contracts/library/ERC2981PerTokenRoyalties.sol
@@ -1,63 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Context.sol";
 import "./ERC2981.sol";
 
 /// @dev This is a contract used to add ERC2981 support to ERC721 and 1155
 abstract contract ERC2981PerTokenRoyalties is ERC2981 {
+    // map of known royalties; tokenID => RoyaltyInfo; #0 => collection-wide royalty
     mapping(uint256 => RoyaltyInfo) internal _royalties;
 
-    function setDefaultRoyalty(address _recipient, uint16 _value)
-        external
-        override
-    {
-        _setDefaultRoyalty(_recipient, _value);
-    }
-
-    function setTokenRoyalty(
-        uint256 _tokenId,
-        address _receiver,
-        uint16 _value
-    ) external override {
-        _setTokenRoyalty(_tokenId, _receiver, _value);
-    }
-
     /// @dev Sets token royalties
-    /// @param tokenId the token id fir which we register the royalties
-    /// @param recipient recipient of the royalties
-    /// @param value percentage (using 2 decimals - 10000 = 100, 0 = 0)
+    /// @param _tokenId the token id for which we register the royalties
+    /// @param _recipient recipient of the royalties
+    /// @param _value percentage (using 2 decimals - 10000 = 100, 0 = 0)
     function _setTokenRoyalty(
-        uint256 tokenId,
-        address recipient,
-        uint256 value
+        uint256 _tokenId,
+        address _recipient,
+        uint256 _value
     ) internal {
-        require(value <= 10000, "Royalty Too high");
-        _royalties[tokenId] = RoyaltyInfo(recipient, uint24(value));
+        require(_value <= 10000, "ERC2981PerTokenRoyalties: Royalty Too high");
+
+        RoyaltyInfo memory royalty = _royalties[_tokenId];
+        require(royalty.recipient == address(0), "ERC2981PerTokenRoyalties: Royalty already set");
+
+        _royalties[_tokenId] = RoyaltyInfo(_recipient, uint24(_value));
+    }
+
+    /// @dev Sets a new royalty recipient for the given tokenID. Only existing recipient can make the change.
+    function setTokenRoyaltyRecipient(uint256 _tokenId, address _recipient) external {
+        // the royalty must be set and the caller must be the current recipient of the royalty
+        RoyaltyInfo memory royalty = _royalties[_tokenId];
+        require(royalty.recipient != address(0) && royalty.recipient == msg.sender, "ERC2981PerTokenRoyalties: Current recipient only");
+
+        _royalties[_tokenId].recipient = _recipient;
     }
 
     /// @dev Sets collection-wide royalty
-    /// @param recipient recipient of the royalties
-    /// @param value percentage (using 2 decimals - 10000 = 100, 0 = 0)
-    function _setDefaultRoyalty(address recipient, uint256 value) internal {
-        require(value <= 10000, "Royalty: Too high");
-        _royalties[0] = RoyaltyInfo(recipient, uint24(value));
+    /// @param _recipient recipient of the royalties
+    /// @param _value percentage (using 2 decimals - 10000 = 100, 0 = 0)
+    function _setDefaultRoyalty(address _recipient, uint256 _value) internal {
+        require(_value <= 10000, "ERC2981PerTokenRoyalties: Royalty too high");
+        _royalties[0] = RoyaltyInfo(_recipient, uint24(_value));
     }
 
-    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
-        external
-        view
-        override
-        returns (address _receiver, uint256 _royaltyAmount)
-    {
+    /// @dev Provides value and recipient for a royalty of the given token and for the given price.
+    /// @param _tokenId the token id fir which we register the royalties
+    /// @param _salePrice The base amount used to calculate the royalty.
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) external view override returns (address _receiver, uint256 _royaltyAmount) {
         RoyaltyInfo memory royalty = _royalties[_tokenId];
 
+        // fallback to collection-wide royalty, if set
         if (royalty.recipient == address(0)) {
-            royalty = _royalties[0]; // use collection-wide royalty
+            royalty = _royalties[0];
         }
 
+        // do the math and return results
         _receiver = royalty.recipient;
         _royaltyAmount = (_salePrice * royalty.amount) / 10000;
-
         return (_receiver, _royaltyAmount);
     }
 }
--- contracts/library/IERC2981RoyaltySetter.sol
+++ contracts/library/IERC2981RoyaltySetter.sol
@@ -5,15 +5,11 @@ import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 interface IERC2981RoyaltySetter is IERC165 {
     // bytes4(keccak256('setDefaultRoyalty(address,uint16)')) == 0x4331f639
     // bytes4(keccak256('setTokenRoyalty(uint256,address,uint16)')) == 0x78db6c53
-
     // => Interface ID = 0x4331f639 ^ 0x78db6c53 == 0x3bea9a6a
 
-    function setDefaultRoyalty(address _receiver, uint16 _royaltyPercent)
-        external;
+    // Set collection-wide default royalty.
+    function setDefaultRoyalty(address _receiver, uint16 _royaltyPercent) external;
 
-    function setTokenRoyalty(
-        uint256 _tokenId,
-        address _receiver,
-        uint16 _royaltyPercent
-    ) external;
+    // Set royalty for the given token.
+    function setTokenRoyalty(uint256 _tokenId, address _receiver, uint16 _royaltyPercent) external;
 }
